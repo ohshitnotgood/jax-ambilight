e49edc4ad71c5392bcca07e687e5a17ebb52781e
ohshitnotgood <praanto@icloud.com>
2024-07-16 21:05:38 +0200

    Replaced Kotlin multiplatform with Tauri

---
+++ desktop/src/components/ipc.ts
@@ -0,0 +1,17 @@
+import net from "node:net"
+
+const path = "/tmp/jr.sock"
+const server = net.createConnection(path)
+
+export function sendMessage() {
+    // TODO: Implement
+}
+
+
+export function onMessageReceived() {
+    // TODO: implement
+}
+
+export function onConnection() {
+    // TODO: implement
+}
