d7565d755028ca9855549720b2bb79ec363045f7
ohshitnotgood <praanto@icloud.com>
2024-08-01 18:46:20 +0200

    Now supports changing zone from frontend

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,8 @@ class MainController:
         self.verbose = False
         self.verbose = parser.parse_args().verbose
         self.server = USocket(verbose=self.verbose)
+        self.n_height_zones = 4
+        self.n_width_zones = 8
         
     def main_loop(self):
         self.server.wait_for_client()
@@ -38,23 +40,13 @@ class MainController:
                 self.server.kill_server()
                 break
         
-    def message_loop(self):
-        try:
-            while True:
-                msg = self.server.wait_for_incoming_msg()
-                if self.verbose: print(f"Received message {msg}")
-                if msg == "kill_srvr":
-                    self.server.send_message("kill_srvr")
-                    self.server.close_connection()
-                elif msg == "ack_ok":
-                    screen_colours = c_colours(n_height_zones=4, n_width_zones=8)
-                    self.server.send_message(str(screen_colours).encode())
-                else:
-                    self.server.send_message(msg)
-        except KeyboardInterrupt or BrokenPipeError:
-            if self.verbose: print("Encountered KeyboardInterrupt or client disconnected, closing server")
-            self.server.close_connection()
-        
+    def update_zones(self, msg):
+        if msg[0:6] == "chg_v:":
+            msg = msg[6:]
+            msg = msg.split(";")
+            self.n_height_zones = msg[0]
+            self.n_height_zones = msg[1]
+    
 
 if __name__ == "__main__":
     mc = MainController()
