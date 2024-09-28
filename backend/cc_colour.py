f6ddebf6eda8d891d90df5ffa1c3abeaba9861c4
ohshitnotgood <praanto@icloud.com>
2024-09-28 20:20:59 +0200

    Added support for windows using pytorch

--- backend/cc_colour.py
+++ backend/cc_colour.py
@@ -1,5 +1,6 @@
-import mss
+import mss, platform
 import jax.numpy as jnp
+import torch
 
 sct = mss.mss()
 def c_colours(n_height_zones: int, n_width_zones: int, inp_img=None, monitor_nr=0, in_colour_space="BRGA"):
@@ -23,7 +24,7 @@ def c_colours(n_height_zones: int, n_width_zones: int, inp_img=None, monitor_nr=
         # width of each zone
         zone_width = int(width / n_width_zones)    
         
-        img = jnp.array(sct_img)
+        img = __array(sct_img)
     else: 
         img = inp_img
         height = img.shape[0]
@@ -35,31 +36,31 @@ def c_colours(n_height_zones: int, n_width_zones: int, inp_img=None, monitor_nr=
     img_trunc_top = img[slice(0, zone_height)]
     img_trunc_bottom = img[slice(height - zone_height, height - 1)]
     
-    img_swapped = jnp.swapaxes(img, 1, 0)
+    img_swapped = __swapaxes(img, 1, 0)
     
     img_trunc_left = img_swapped[slice(0, zone_width)]
     img_trunc_right = img_swapped[slice(width - zone_width, width - 1)]
     
     
     # Average across each section
-    img_line_avg_top = jnp.average(img_trunc_top, (0))
-    img_line_avg_bottom = jnp.average(img_trunc_bottom, (0))
-    img_line_avg_left = jnp.average(img_trunc_left, (0))
-    img_line_avg_right = jnp.average(img_trunc_right, (0))
+    img_line_avg_top = __average(img_trunc_top, (0))
+    img_line_avg_bottom = __average(img_trunc_bottom, (0))
+    img_line_avg_left = __average(img_trunc_left, (0))
+    img_line_avg_right = __average(img_trunc_right, (0))
     
     
     # Reshape the image matrix into 4 zones.
-    img_reshaped_top = jnp.reshape(img_line_avg_top, (n_width_zones, zone_width, n_colour_channels))
-    img_reshaped_bottom = jnp.reshape(img_line_avg_bottom, (n_width_zones, zone_width, n_colour_channels))
-    img_reshaped_left = jnp.reshape(img_line_avg_left, (n_height_zones, zone_height, n_colour_channels))
-    img_reshaped_right = jnp.reshape(img_line_avg_right, (n_height_zones, zone_height, n_colour_channels))
+    img_reshaped_top = __reshape(img_line_avg_top, (n_width_zones, zone_width, n_colour_channels))
+    img_reshaped_bottom = __reshape(img_line_avg_bottom, (n_width_zones, zone_width, n_colour_channels))
+    img_reshaped_left = __reshape(img_line_avg_left, (n_height_zones, zone_height, n_colour_channels))
+    img_reshaped_right = __reshape(img_line_avg_right, (n_height_zones, zone_height, n_colour_channels))
     
     
     # Get the average colour in each zone.
-    img_zone_avg_top = jnp.average(img_reshaped_top, (1))
-    img_zone_avg_bottom = jnp.average(img_reshaped_bottom, (1))
-    img_zone_avg_left = jnp.average(img_reshaped_left, (1))
-    img_zone_avg_right = jnp.average(img_reshaped_right, (1))
+    img_zone_avg_top = __average(img_reshaped_top, (1))
+    img_zone_avg_bottom = __average(img_reshaped_bottom, (1))
+    img_zone_avg_left = __average(img_reshaped_left, (1))
+    img_zone_avg_right = __average(img_reshaped_right, (1))
     
     # Convert the BGRA array into desired array type
     rgb_top = _convert_brga_array_to_rgb_array(img_zone_avg_top, in_color_space=in_colour_space)
@@ -69,15 +70,33 @@ def c_colours(n_height_zones: int, n_width_zones: int, inp_img=None, monitor_nr=
     
     return [rgb_top, rgb_bottom, rgb_left, rgb_right]
     
+def __average(inp, axis):
+    if platform.system() == 'Linux':
+        return jnp.average(inp, axis)
+    elif platform.system() == 'Windows':
+        return torch.mean(inp, axis)
+    else: raise OSError("Operating system not supported")
+    
+def __reshape(inp, new_shape):
+    if platform.system() == 'Linux':
+        return jnp.reshape(inp, new_shape)
+    elif platform.system() == 'Windows':
+        return torch.reshape(inp, new_shape)
+    else: raise OSError("Operating system not supported")
 
-def _convert_brga_array_to_ycrcb_array(jax_ar):
-    y_const = jnp.array([0.299, 0.587, 0.144])
-    u_const = jnp.array([-0.14713, -0.28886, 0.436])
-    v_const = jnp.array([0.615, -0.51499, -0.10001])
-    y = jnp.dot(jax_ar, y_const)
-    u = jnp.dot(jax_ar, u_const)
-    v = jnp.dot(jax_ar, v_const)
-    return jnp.array([y, u, v])
+def __swapaxes(inp, axis1, axis2):
+    if platform.system() == 'Linux':
+        return jnp.swapaxes(inp, axis1, axis2)
+    elif platform.system() == 'Windows':
+        return torch.swapaxes(inp, axis1, axis2)
+    else: raise OSError("Operating system not supported")
+    
+def __array(inp):
+    if platform.system() == 'Linux':
+        return jnp.array(inp)
+    elif platform.system() == 'Windows':
+        return torch.tensor(inp, requires_grad=False)
+    else: raise OSError("Operating system not supported")
 
 
 def _convert_brga_array_to_rgb_array(jax_ar, in_color_space="RGB"):
@@ -97,5 +116,3 @@ def _convert_brga_array_to_rgb_array(jax_ar, in_color_space="RGB"):
 
 if __name__ == "__main__":
     print(c_colours(3, 4))
-    test_ar = jnp.array([61, 49, 47])
-    print(_convert_brga_array_to_ycrcb_array(test_ar))
